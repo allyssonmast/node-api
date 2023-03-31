2184dd4b0423008c9ab067b49279c88ed82f8e35
Allysson Mastrangelo <allysson-m@hotmail.com>
2023-03-31 12:17:57 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  testEnviromen: "node",
+
+  collectCoverage: true,
+
+  coverageDirectory: "coverage",
+
+  coverageProvider: "v8",
+};
