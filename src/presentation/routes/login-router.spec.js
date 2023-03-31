fe2bb8a6c0388aeb15284670f0eef018740636a9
Allysson Mastrangelo <allysson-m@hotmail.com>
2023-03-31 13:55:51 -0300

    add test to LoginRouter (email and password)

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route(httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400,
       };
@@ -13,6 +13,18 @@ describe("Login Router", () => {
     const httpRequest = {
       body: {
         password: "any_password",
+      },
+    };
+
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+
+  test("Shold return 400 if no password provided", () => {
+    const sut = new LoginRouter();
+    const httpRequest = {
+      body: {
+        email: "email@email.com",
       },
     };
 
