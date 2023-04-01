9a43f8c01a6407e0d14f6bb8aa0957bde7f11f42
Allysson Mastrangelo <allysson-m@hotmail.com>
2023-04-01 18:40:25 -0300

    finish email-validator

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,14 +1,12 @@
+const EmailValidator = require('../utils/email-validator')
 const validator = require('validator')
-
-class EmailValidator {
-  isValid (email) {
-    return validator.isEmail(email)
-  }
+const makeSut = () => {
+  return new EmailValidator()
 }
 
 describe('Email Validator', () => {
   test('should return true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
 
     const isEmailValid = sut.isValid('valid@hotmail.com')
 
@@ -17,10 +15,18 @@ describe('Email Validator', () => {
 
   test('should return false if validator returns false', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
 
     const isEmailValid = sut.isValid('valid')
 
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Shold call validator with correct email', () => {
+    const sut = makeSut()
+
+    sut.isValid('valid')
+
+    expect(validator.email).toBe('valid')
   })
 })
